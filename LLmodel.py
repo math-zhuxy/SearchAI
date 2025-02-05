c58c3d772ba94c3b04096516fe7c37fc006d411d
math-zhuxy <235934429@qq.com>
2025-02-05 14:46:17 +0800

    修改了setting.json格式

--- LLmodel.py
+++ LLmodel.py
@@ -1,18 +1,23 @@
-import WebCrawler
 import json
 import requests
-AllMessages = []
-with open("setting.json", "r", encoding= "utf-8") as file:
-    setting_data = json.load(file)
-if "api_key" not in setting_data or "url" not in setting_data or "prompt" not in setting_data:
-    print("setting.json file format error")
-AllMessages.append(
-    {
-        "role": "system",
-        "content": setting_data["prompt"]
-    }
-)
+try:
+    with open("setting.json", "r", encoding= "utf-8") as file:
+        setting_data = json.load(file)
+    system_message = setting_data["prompt"]["sys"]
+    model_url = setting_data["model"]["url"]
+    user_api_key = setting_data["model"]["apikey"]
+    model_name = setting_data["model"]["name"]
+except FileNotFoundError as e:
+    print(f"file \"setting.json\" not found: {e} ")
+except json.JSONDecodeError as e:
+    print(f"setting.json format is wrong: {e}")
 def communicate(message: str)-> str:
+    AllMessages = [
+        {
+            "role": "system",
+            "content": system_message
+        }
+    ]
     AllMessages.append(
         {
             "role": "user",
@@ -21,13 +26,16 @@ def communicate(message: str)-> str:
     )
     req_header = {
         "Content-Type": "application/json",
-        'Authorization': f'Bearer {setting_data["api_key"]}'
+        'Authorization': f'Bearer {user_api_key}'
     }
     req_data = {
-        "model": "glm-4-flash",
+        "model": model_name,
         "messages": AllMessages
     }
-    response = requests.post(url= setting_data["url"], headers=req_header, data=json.dumps(req_data))
-    print(response.json())
+    response = requests.post(url= model_url, headers=req_header, data=json.dumps(req_data))
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print("error")
 
 communicate("你是谁")
