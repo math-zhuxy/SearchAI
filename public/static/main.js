becc1ee17d154277a5ba04cfe55b1b25d090aadd
math-zhuxy <235934429@qq.com>
2025-02-09 17:19:12 +0800

    前端页面初步完善

--- public/static/main.js
+++ public/static/main.js
@@ -1,1 +1,27 @@
-document.getElementById("message")
+
+
+
+var sendButton = document.getElementById("input_button");
+var text_input = document.getElementById("text_input");
+
+function CreatePopupbox(txt, duration){
+    const popup_box = document.getElementById("popup-box");
+    popup_box.classList.add("visible");
+    popup_box.textContent = txt;
+    setTimeout(() => {
+        popup_box.classList.remove("visible");
+    }, duration);
+}
+
+sendButton.addEventListener('click', function() {
+    if(text_input.value === ""){
+        CreatePopupbox("输入文本不能为空", 1500);
+        return;
+    }
+
+    this.style.backgroundImage = 'url("static/img/stop.png")';
+
+    text_input.value = "";
+    text_input.disabled = true;
+    text_input.style.cursor = "not-allowed";
+});
