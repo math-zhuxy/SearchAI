d9e63d1d7bb164b641b56d3786598aca2989c241
math-zhuxy <235934429@qq.com>
2025-02-10 15:12:36 +0800

    前端页面初步完成

--- public/static/main.js
+++ public/static/main.js
@@ -1,6 +1,3 @@
-
-
-
 var sendButton = document.getElementById("input_button");
 var text_input = document.getElementById("text_input");
 
@@ -13,15 +10,41 @@ function CreatePopupbox(txt, duration){
     }, duration);
 }
 
+function addMessage(IsUser, txt){
+    const msg_container = document.getElementById("message_list");
+    if(IsUser === true) {
+        const msg_ele = `
+        <div class="user_msg">
+            <p class="user_msg_content">${txt}</p>
+            <img src="static/img/stop.png" alt="User Avatar" class="avatar">
+        </div>
+        `
+        msg_container.innerHTML += msg_ele;
+    }
+    else {
+        const msg_ele =`
+        <div class="ai_msg">
+            <img src="static/img/send-message.png" alt="AI Avatar" class="avatar">
+            <p class="ai_msg_content">${txt}</p>
+        </div>
+        `
+        msg_container.innerHTML += msg_ele;
+    }
+}
+
 sendButton.addEventListener('click', function() {
-    if(text_input.value === ""){
+    if(text_input.value === "") {
         CreatePopupbox("输入文本不能为空", 1500);
         return;
     }
 
-    this.style.backgroundImage = 'url("static/img/stop.png")';
-
+    addMessage(true, text_input.value)
+    
+    addMessage(false, "请稍后再试")
+    
     text_input.value = "";
+
+    this.style.backgroundImage = 'url("static/img/stop.png")';
     text_input.disabled = true;
     text_input.style.cursor = "not-allowed";
 });
